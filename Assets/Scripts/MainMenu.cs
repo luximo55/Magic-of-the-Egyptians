263d120f5b280ff69a512a9416b19269cd775eb1
1Angel2 <129875102+1Angel2@users.noreply.github.com>
2024-04-04 19:54:39 +0200

    MainMenu
    Sve napravio, osim settings. Dosta za danas, oču si kosu isčupati

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ public void Play()
+ {
+    SceneManager.LoadScene("Level1");
+ }
+
+ public void Exit()
+ {
+    Application.Quit();
+ }
 }
