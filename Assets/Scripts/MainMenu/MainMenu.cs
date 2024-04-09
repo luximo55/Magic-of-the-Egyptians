6ac78da2cbb771f54db561dbec6bdd4e4d5aaabe
luximo55 <96381767+luximo55@users.noreply.github.com>
2024-04-09 17:17:24 +0200

    Fixed a few bugs, modified level1, at the end with level3

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,17 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Data.SqlTypes;
-using System.Runtime.CompilerServices;
-using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+
 public class MainMenu : MonoBehaviour
 {
 
  public void Play()
  {
-    SceneManager.LoadScene("Level1");
+    SceneManager.LoadScene(1);
  }
 
  public void Exit()
